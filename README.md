3e3c36ce0706d883c8204fdc84de3e6ccd48a154
marcosregato <marcosregato01@gmail.com>
2021-10-04 01:03:34 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# projetoFaculdade
+ # "avrdude: ser_open(): can't open device "/dev/ttyACM0": Permission denied"
+ 
+ # sudo apt install --reinstall arduino
+ 
+ 
+ # caused by processing.app.serialexception error touching serial port '/dev/ttyacm0'
+ 
+ 
+ # sudo chmod 666 /dev/ttyACM0
---
+++ src/tendao/Tendao.cpp
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+// construtor
+Tendao::Tendao(int *pos, Servo *servo_9){
+    int pos = &pos;
+    Servo &servo_9;
+}
+
+/*void setup(){
+  servo_9.attach(9, 500, 2500);
+}*/
+
+void movimentoServo(){
+    servo_9.attach(9, 500, 2500);
+    // varre o servo de 0 a 180 graus em etapas
+    // de 1 grau 
+  for (pos = 0; pos <= 180; pos += 1) {
+    // diga ao servo para ir para a posição na variável 'pos' 
+    servo_9.write(pos);
+    // espera 15 ms para o servo alcançar a posição
+    delay(15); // Aguarde 15 milissegundos (s) 
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    // tell servo to go to position in variable 'pos'
+    servo_9.write(pos);
+    // wait 15 ms for servo to reach the position
+    delay(15); // Aguarde 15 milissegundos (s) 
+  }
+}
